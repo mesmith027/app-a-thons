492d540ae6b6d4879212ed976b21632b8fe817d4
Marisa Smith <marisa@streamlit.io>
2021-08-19 16:07:05 -0400

    Markdown cells displaying properly

--- 2021/august/app.py
+++ 2021/august/app.py
@@ -1,5 +1,33 @@
 import streamlit as st
+import json
 
-st.title("Jupyter Notebook to Streamlit App")
+st.title("Jupyter Notebook to Streamlit App 🥳")
 
-file = st.file_uploader("Jupyter Notebook file", help=".ipynb extentions only")
+file = st.file_uploader("Jupyter Notebook file", help=".ipynb extentions only", type='ipynb')
+
+if file is not None:
+    json_data = json.load(file)
+    #st.write(json_data["metadata"], len(json_data))
+    #st.write(json_data.keys())
+
+    #st.write(json_data["cells"])
+
+    num_cells = len(json_data["cells"])
+    st.write("Your notebook has",num_cells,"cells")
+
+    for x in range(num_cells):
+        st.write(x)
+        #st.write(json_data["cells"][x])
+
+        if json_data["cells"][x]["cell_type"] == "markdown":
+            #st.write(len(json_data["cells"][x]["source"]))
+            for y in range(len(json_data["cells"][x]["source"])):
+                st.markdown(json_data["cells"][x]["source"][y])
+        elif json_data["cells"][x]["cell_type"] == "code":
+            st.write(len(json_data["cells"][x]["source"]))
+            for y in range(len(json_data["cells"][x]["source"])):
+                with st.echo():
+                    json_data["cells"][x]["source"][y]
+        else:
+            st.write("unknown cell type")
+            st.write(json_data["cells"][x]["cell_type"])
