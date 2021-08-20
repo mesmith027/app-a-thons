9ad12fe6ce9293a6dea0798260efd382fc4ca24f
Marisa Smith <marisa@streamlit.io>
2021-08-20 14:16:47 -0400

    code displaying properly

--- 2021/august/app.py
+++ 2021/august/app.py
@@ -1,40 +1,33 @@
 import streamlit as st
 import json
+from streamlit_ace import st_ace
 
 st.title("Jupyter Notebook to Streamlit App 🥳")
 
 file = st.file_uploader("Jupyter Notebook file", help=".ipynb extentions only", type='ipynb')
 
+file
 if file is not None:
     json_data = json.load(file)
-    #st.write(json_data["metadata"], len(json_data))
-    #st.write(json_data.keys())
-
-    #st.write(json_data["cells"])
 
     num_cells = len(json_data["cells"])
     st.write("Your notebook has",num_cells,"cells")
 
-    full_code = []
+    full_code = ""
 
     for x in range(num_cells):
-        #st.write(x)
-        #st.write(json_data["cells"][x])
 
         if json_data["cells"][x]["cell_type"] == "markdown":
-            #st.write(len(json_data["cells"][x]["source"]))
             for y in range(len(json_data["cells"][x]["source"])):
                 st.markdown(json_data["cells"][x]["source"][y])
         elif json_data["cells"][x]["cell_type"] == "code":
-            #st.write(len(json_data["cells"][x]["source"]))
             for y in range(len(json_data["cells"][x]["source"])):
-                #st.write(json_data["cells"][x]["source"][y])
-                st.code(json_data["cells"][x]["source"][y])
-                full_code = full_code.append(json_data["cells"][x]["source"][y])
-                # with st.echo():
-                #     json_data["cells"][x]["source"][y]
-            with st.echo():
-                full_code
+                full_code = full_code + json_data["cells"][x]["source"][y]
+
+            #code = st_ace(full_code,wrap=True, font_size=15,theme="dawn",language="python")
+            #exec(code)
+            st.code(full_code)
+            #exec(full_code)
         else:
-            st.write("unknown cell type")
+            st.write("sorry unknown cell type")
             st.write(json_data["cells"][x]["cell_type"])
