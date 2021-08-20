d17bc889fce5d168cc4098e95bee8032686e9cb0
Marisa Smith <marisa@streamlit.io>
2021-08-20 14:50:30 -0400

    cleaning up app layout

--- 2021/august/app.py
+++ 2021/august/app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 import json
 from streamlit_ace import st_ace
+from PIL import Image
+
+logo = Image.open("jupyter.jpeg")
+st.set_page_config(page_title= "Jupyter Notebook Reader",layout="wide", page_icon=logo)
 
 st.title("Jupyter Notebook to Streamlit App 🥳")
 
-file = st.file_uploader("Jupyter Notebook file", help=".ipynb extentions only", type='ipynb')
+with st.sidebar:
+    file = st.file_uploader("Jupyter Notebook file", help=".ipynb extentions only", type='ipynb')
+    "Uploaded File:",file
+    example = st.checkbox("Use example .ipynb file")
+
+if example:
+    file = open("test_JN.ipynb")
 
 file
 if file is not None:
